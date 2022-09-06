d5bfd227546e933b64001f9a864d4dfedb8ecb4f
katherinex <kathexio@gmail.com>
2022-09-05 21:27:23 -0500

    Operaciones matematicas alternativas con menú python

--- GonzalezK-EjecutarFunciones.py
+++ GonzalezK-EjecutarFunciones.py
@@ -8,7 +8,8 @@
 # [18.07.2020]
 # ----------------------------------------------------------------------------------------
 # IMPORTAR MODULOS
-from operacionesAlternativas import * # modulo de python para este ejemplo (se usara para mostrar la fecha)
+from operacionesAlternativas import *  # modulo de python para este ejemplo (se usara para mostrar la fecha)
+
 # https://www.delftstack.com/es/howto/python/python-import-from-parent-directory/#:~:text=Para%20importar%20un%20m%C3%B3dulo%20usando,el%20enfoque%20de%20paquete%20relativo.
 # ----------------------------------------------------------------------------------------
 # VARIABLES GLOBALES Y PRE-CONDICIONES
@@ -30,33 +31,41 @@ from operacionesAlternativas import * # modulo de python para este ejemplo (se u
 # <<Escriba desde aqui el código del programa...>>
 opcion = ''
 
-def validarInput (mensaje):
+
+def validarInput(mensaje):
     seguirPreguntando = True
     while seguirPreguntando:
         opcionIngresada = input(mensaje)
-        if (opcionIngresada == '1' or '2' or '3' or '4' or '0'):
+        if (opcionIngresada == '1' or
+                opcionIngresada == '2' or
+                opcionIngresada == '3' or
+                opcionIngresada == '4' or
+                opcionIngresada == '0'):
             seguirPreguntando = False
         else:
             print('El valor ingresado no es válido')
     return opcionIngresada
 
+
 while (opcion != '0'):
     print('\n')
     print('Seleccione una opción:')
-    print('1. Digitos por derecha')
-    print('2. Digitos por izquierda')
-    print('3. Multiplicaciòn Rusa')
+    print('1. Dígitos por derecha')
+    print('2. Dígitos por izquierda')
+    print('3. Multiplicación Rusa')
     print('4. Máximo Común Divisor')
     print('0. Salir')
     opcion = validarInput('Ingrese opción: ')
 
+    print('\n')
     if (opcion == '1'):
         digitosPorDerecha()
-    if (opcion == '2'):
+    elif (opcion == '2'):
         digitosPorIzquierda()
-
-
-
+    elif (opcion == '3'):
+        multiplicacionRusa()
+    elif (opcion == '4'):
+        maxCD()
 
 # ----------------------------------------------------------------------------------------
 # end.
--- operacionesAlternativas.py
+++ operacionesAlternativas.py
@@ -35,8 +35,8 @@ def validarInput (mensaje, condicion):
 def esNumero(numero):
     return numero.isdigit()
 
-def numeroIngresado():
-    numero = validarInput('Ingrese un número entero: ', esNumero)
+def numeroIngresado(mensaje):
+    numero = validarInput(mensaje, esNumero)
     return int(numero)
 # ----------------------------------------------------------------------------------------
 # FUNCIÓN: <<nombre de la función 1>>
@@ -66,7 +66,7 @@ def digitosDeNumero(numero):
 # ----------------------------------------------------------------------------------------
 def digitosPorIzquierda():
     print('Dígitos por Izquierda: ')
-    numero = numeroIngresado()
+    numero = numeroIngresado('Ingrese un número entero: ')
     print('Los dígitos del número ' + str(numero) + ' de izquierda a derecha son: ')
     for digito in digitosDeNumero(numero)[::-1]:
         print(digito)
@@ -83,7 +83,7 @@ def digitosPorIzquierda():
 
 def digitosPorDerecha():
     print('Dígitos por Derecha: ')
-    numero = numeroIngresado()
+    numero = numeroIngresado('Ingrese un número entero: ')
     print('Los dígitos del número ' + str(numero) + ' de derecha a izquierda son: ')
     for digito in digitosDeNumero(numero):
         print(digito)
@@ -99,8 +99,25 @@ def digitosPorDerecha():
 # ----------------------------------------------------------------------------------------
 
 def multiplicacionRusa():
-    print('multiplicación Rusa')
+    print('Multiplicación Rusa: ')
+    multiplicador = numeroIngresado('Ingrese el multiplicador (debe ser un numero entero): ')
+    multiplicando = numeroIngresado('Ingrese el multiplicando (debe ser un numero entero): ')
+    multiplicacion = str(multiplicador) + 'x' + str(multiplicando)
 
+    sumaMultiplicandos = 0
+
+    while (multiplicador != 1):
+
+        if (multiplicador % 2 != 0):
+            sumaMultiplicandos = sumaMultiplicandos + multiplicando
+
+        multiplicando = multiplicando * 2
+        multiplicador = multiplicador // 2
+
+    if (multiplicador == 1):
+        sumaMultiplicandos = sumaMultiplicandos + multiplicando
+
+    print('El resultado de la multiplicación Rusa de ', multiplicacion, ' es: ', str(sumaMultiplicandos))
 # ----------------------------------------------------------------------------------------
 # FUNCIÓN: <<nombre de la función 2>> ... Y ASI PARA TODAS LAS FUNCIONES ...
 # ----------------------------------------------------------------------------------------
